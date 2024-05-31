213e807296ad88df8414f233ae035e7f7c575987
ManuelCastSant <manu.casa1998@gmail.com>
2024-05-31 00:03:09 -0600

    yes

--- src/componets/Hero/Hero.jsx
+++ src/componets/Hero/Hero.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { getImageUrl } from '../../utils';
+import styles from "./Hero.module.css";
+
 
 export const Hero = () => {
   return (
-    <section>
-        <div>
-            <h1>Hola, Soy Manuel Castañeda</h1>
-            <p>
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hola, Soy Manuel Castañeda</h1>
+            <p className={styles.description}>
             Soy Ingeniero en Sistemas Computacionales y especializado en Full-Stack Developer
             </p>
-            <a href="mailto:mike.nike335@gmail.co">Contactame</a>
+            <a href="mailto:mike.nike335@gmail.co" className={styles.contacBtn}>Contactame</a>
         </div>
-        <img src={getImageUrl("hero/heroImage.png")} alt="" />
-
+        <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.HeroImg} />
+        <div className={styles.topBlur}/>
+        <div className={styles.bottonBlur}/>
     </section>
   )
 };
